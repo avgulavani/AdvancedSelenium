1442e196b5737d8c7c7a42ce9dc57302ad3895e8
Ashish <gulavaniashish2008@gmail.com>
2020-12-14 21:40:19 +0530

    added more logic to support headless execution

--- src/main/java/stepDefinations/openbrowserStepDefinition.java
+++ src/main/java/stepDefinations/openbrowserStepDefinition.java
@@ -1,11 +1,17 @@
 package stepDefinations;
 
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.commons.logging.Log;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -20,23 +26,38 @@ public class openbrowserStepDefinition {
 	    @Given("^Open the Firefox and launch the application$")					
 	    public void open_the_Firefox_and_launch_the_application() throws Throwable							
 	    {		
+	    	 ChromeOptions chromeOptions=new ChromeOptions();
 	       System.setProperty("webdriver.chrome.driver", "/Users/a.vitthal.gulavani/git/ipt-ie-automation-tests/chromedriver");					
-	       ChromeOptions chromeOptions=new ChromeOptions();
+	       DesiredCapabilities cap = DesiredCapabilities.chrome();
+	        cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+	        cap.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+	        chromeOptions.addArguments("start-maximized");
+	        chromeOptions.addArguments("--always-authorize-plugins");
+	        chromeOptions.setAcceptInsecureCerts(false);
+	        chromeOptions.setExperimentalOption("useAutomationExtension", false);
+	        chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+
+	      
 	       chromeOptions.addArguments("--disable-gpu");
 	        chromeOptions.addArguments("--disable-extensions");
 	        chromeOptions.addArguments("--no-sandbox");
 	        chromeOptions.addArguments("--disable-dev-shm-usage");
+	        
 	        chromeOptions.addArguments("--headless");
-	        chromeOptions.addArguments("--window-size=1580,1280");
-	        chromeOptions.setHeadless(true);
+	        chromeOptions.setAcceptInsecureCerts(true);
+	        chromeOptions.addArguments("--disable-dev-shm-usage");
+            chromeOptions.addArguments("--window-size=1600,1200");
+	     
 	       
-	       driver= new ChromeDriver	();					
-	       driver.manage().window().maximize();			
-	       Log.info("Opening browser");
+	       driver= new ChromeDriver	(chromeOptions);					
+	       driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+	        Dimension d = new Dimension(1500,2500);
+	        driver.manage().window().setSize(d);
+	       
 	       driver.get("http://demo.guru99.com/v4");					
 	    }		
 
-	    @When("^Enter the Username and Password$")					
+	    @When("^Enter the User	name and Password$")					
 	    public void enter_the_Username_and_Password() throws Throwable 							
 	    {		
 	       driver.findElement(By.name("uid")).sendKeys("username12");							
